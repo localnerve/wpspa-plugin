7ca2c7e4b53f3c3307ff644091c8780fb70e525b
localnerve <alex@localnerve.com>
2014-02-15 18:01:30 -0500

    fix import_wp_post action to exclude child link info from nav_menu_items

--- wpspa/actions/query/class.wpspa-query.php
+++ wpspa/actions/query/class.wpspa-query.php
@@ -2,13 +2,20 @@
 
 class WPSPA_Query {
 
+  // looks this might get covered in WP 3.9
+  // http://wpseek.com/is_nav_menu_item/
+  protected static function is_nav_menu_item($wp_post) {    
+    return ( ! is_wp_error( $wp_post->ID ) && ( 'nav_menu_item' == $wp_post->post_type ) );
+  }
+
   // The handler for the json_api_import_wp_post action
   public static function import_wp_post($json_api_post, $wp_post) {
     $custom_fields = $json_api_post->custom_fields;
-    if (isset($wp_post->child)) {      
+    if ( isset($wp_post->child) ) {
       $custom_fields->_wpspa_child_link = $wp_post->child;
     } else {
-      $custom_fields->_wpspa_child_link = false;
+      if ( !WPSPA_Query::is_nav_menu_item($wp_post) )
+        $custom_fields->_wpspa_child_link = false;
     }
   }
 
